eea8b508c3a9dfcc5e6660aca34709ad07e33c9a
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-10-03 20:38:23 +0600

    Module 52.1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router = createBrowserRouter([
+    {path: '/', element: <div>This is Default Page</div>},
+    {path: '/home', element: <div>This is Home Page</div>},
+    {path: '/about', element: <div>This is about page</div>}
+  ])
 
   return (
-    console.log("lmao")
+    <RouterProvider router={router}></RouterProvider>
   );
 }
 
